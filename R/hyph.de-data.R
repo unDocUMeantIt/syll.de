c3ad03721c23a00cf68267dcea514669152621bf
unDocUMeantIt <m@reaktanz.de>
2017-10-03 14:45:48 +0200

    released as 0.1-2

--- R/hyph.de-data.R
+++ R/hyph.de-data.R
@@ -52,4 +52,10 @@
 #' [1] \url{http://tug.ctan.org/tex-archive/language/hyph-utf8/tex/generic/hyph-utf8/patterns/}
 #'
 #' [2] \url{http://www.ctan.org/tex-archive/macros/latex/base/lppl.txt}
+#' @examples
+#' \dontrun{
+#' library(sylly.de)
+#' sampleText <- c("Dies","ist","ein","ziemlich","dummes","Beispiel")
+#' hyphen(sampleText, hyph.pattern="de")
+#' }
 NULL
